8781194fa37c0b9c0a852e9f8566e4063112fa5e
Collin Eddy <linemouth@gmail.com>
2023-08-05 12:22:36 -0700

    Added Vector2.Rotate().

--- Extensions/Vector2Extensions.cs
+++ Extensions/Vector2Extensions.cs
@@ -10,5 +10,18 @@ namespace Utils.Unity
     {
         public static float Cross(this Vector2 a, Vector2 b) => (a.x * b.y) - a.y * b.x;
         public static float TaxiDistance(this Vector2 v) => Mathf.Abs(v.x) + Mathf.Abs(v.y);
+        /// <summary>Rotates a direction vector by some angle.</summary>
+        /// <param name="v">A Vector2 to rotate.</param>
+        /// <param name="angle">Angle by which to rotate, in degrees.</param>
+        public static Vector2 Rotate(this Vector2 v, float angle)
+        {
+            float radians = (float)Math.DegToRad * angle;
+            float sin = Math.Sin(radians);
+            float cos = Math.Cos(radians);
+            return new Vector2(
+                cos * v.x - sin * v.y,
+                sin * v.x + cos * v.y
+            );
+        }
     }
 }
