e4731e199bf7aceab25e1e0025cd2dfad365d118
Collin Eddy <linemouth@gmail.com>
2023-08-05 09:34:25 -0700

    Added Vector2.TaxiDistance().

--- Extensions/Vector2Extensions.cs
+++ Extensions/Vector2Extensions.cs
@@ -9,5 +9,6 @@ namespace Utils.Unity
     public static class Vector2Extensions
     {
         public static float Cross(this Vector2 a, Vector2 b) => (a.x * b.y) - a.y * b.x;
+        public static float TaxiDistance(this Vector2 v) => Mathf.Abs(v.x) + Mathf.Abs(v.y);
     }
 }
